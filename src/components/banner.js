20c8b1bf2738a20a143a6f81b7a4a64dea84af94
Trieu Van Ban <ban21032002@gmaiil.com>
2022-01-16 11:33:54 +0700

    update

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,6 +1,6 @@
 const Banner = {
     render() {
-        return `<img class="mt-[15px] mb-[30px]" src="./img/Banner-fpt.png" alt="">`;
+        return `<img class="mt-[15px] mb-[30px]" src=".../img/Banner-fpt.png" alt="">`;
     },
 };
 export default Banner;
